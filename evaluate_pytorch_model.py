46987a6a0154b7b7ed07a6c4d8a45be644e43c24
Richard Findlay <rfindlay1992@gmail.com>
2021-10-30 23:46:54 +0100

    test script for evaluating pytorch DA price prediction model

---
+++ evaluate_pytorch_model.py
@@ -0,0 +1,45 @@
+# test / sandbox script to test Pytorch model
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+from pickle import load
+import matplotlib.pyplot as plt
+
+from DA_ElectricityPrice import LSTMCNNModel
+
+# load model
+PATH = './Models/da_price_prediction_336hr_in_24hr_out_model.pt'
+
+model = LSTMCNNModel()
+model.load_state_dict(torch.load(PATH, map_location=torch.device('cpu')))
+model.eval()
+
+
+# load test data
+test_load = open("./Data/processed_data/test_data_336hr_in_24hr_out.pkl", "rb") 
+test_data = load(test_load)
+test_load.close()
+
+
+model.eval()
+loss = 0 
+
+inputs = np.moveaxis(test_data['X_test'], -1, 1)
+
+inputs = torch.tensor(inputs, dtype=torch.float64)
+print(test_data['X_test'].shape)
+# exit()
+
+prediction = model(inputs.float())
+exit()
+
+
+with torch.no_grad():
+	for X in test_data['X_test']:
+		# a = np.expand_dims(X,axis=0)
+		x = torch.tensor(np.expand_dims(X,axis=0), dtype=torch.float64)
+		prediction = model(x.float())
+		loss += loss_fn(prediction, y.float())
+	print(f'loss: {loss}')
