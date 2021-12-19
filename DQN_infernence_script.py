5d01fb5a2c040364744340763414a9bc083bf234
Richard Findlay <rfindlay1992@gmail.com>
2021-12-19 23:48:41 +0000

    inference script updated to latest version of environment

--- DQN_infernence_script.py
+++ DQN_infernence_script.py
@@ -8,8 +8,8 @@ import matplotlib.pyplot as plt
 
 from DA_ElectricityPrice import LSTMCNNModel
 from battery_degrade import BatteryDegradation
-from battery_environment import Battery
-from DQN_pytorch import DQN_Agent
+from battery_environment_V2 import Battery
+from DQN_pytorch_V2 import DQN_Agent
 
 # declare environment dictionary
 env_settings = {
@@ -23,11 +23,12 @@ env_settings = {
     'scaler_transform_path': './Data/processed_data/da_price_scaler.pkl',				
     'train_data_path': './Data/processed_data/train_data_336hr_in_24hr_out_unshuffled.pkl', # Path to trian data
     'test_data_path': './Data/processed_data/test_data_336hr_in_24hr_out_unshuffled.pkl',	 # Path to test data
-    'torch_model': './Models/da_price_prediction_336hr_in_24hr_out_model.pt'	 # relevant to current file dir
+    'torch_model': './Models/da_price_prediction_336hr_in_24hr_out_model.pt',	 # relevant to current file dir
+    'price_track': 'true'
 }
 
 
-test_size = 100
+test_size = 1
 time_range = 168
 
 state_size = (25)
@@ -42,7 +43,7 @@ tau = 1
 update = 10000 # 168 best also 100 for hard up date 
 seed = 100
 
-dqn_agent = DQN_Agent(state_size, action_size, learning_rate, buffer_size, gamma, tau, batch_size, seed)
+dqn_agent = DQN_Agent(state_size, action_size, learning_rate, buffer_size, gamma, tau, batch_size, seed, soft_update=False)
 dqn_agent.qnet.load_state_dict(torch.load('checkpoint.pth', map_location=torch.device('cpu')))
 
 
@@ -54,6 +55,11 @@ env = Battery(env_settings)
 total_cumlative_profit = 0
 cumlative_profit = [0]
 
+actions = []
+socs = []
+
+hour = []
+idx = 0
 for test_eps in range(test_size):
 	print(f'day_ref: {test_eps}')
 
@@ -63,11 +69,22 @@ for test_eps in range(test_size):
 
 		action = dqn_agent.action(cur_state)
 
+		socs.append(cur_state[-1])
+		hour.append(idx)
+
 		new_state, reward, done, info = env.step(cur_state, action, step)
+
+		actions.append(info['action_clipped'])
 
 		total_cumlative_profit += info["ts_cost"]
 		cumlative_profit.append(cumlative_profit[-1] + info["ts_cost"])
 
+		cur_state = new_state
+
+		idx += 1 
+
+
+print(actions)
 
 print(total_cumlative_profit)
 
@@ -76,4 +93,9 @@ ax.plot(cumlative_profit, label="cumlative_profit")
 ax.legend()
 ax.grid()
 plt.show()
+
+plt.bar(hour, actions)
+plt.bar(hour, socs)
+plt.show()
+
 
---
+++ DQN_pytorch_V2.py
@@ -0,0 +1,175 @@
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+from collections import namedtuple, deque 
+import random
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# define model architecture
+class QNet(nn.Module):
+	""" Policy Model """
+	def __init__(self, state_size, action_size, seed, fc1_units=16, fc2_units=16, fc3_units=16):
+		super(QNet, self).__init__()
+		self.seed = torch.manual_seed(seed)
+		self.dense1 = nn.Linear(state_size, fc1_units)
+		self.dense2 = nn.Linear(fc1_units, fc2_units)
+		self.dense3 = nn.Linear(fc2_units, fc3_units)
+		self.dense4 = nn.Linear(fc3_units, action_size)
+
+	def forward(self, states):
+		""" map state values to action values """
+		x = F.relu(self.dense1(states))
+		x = F.relu(self.dense2(x))
+		x = F.relu(self.dense3(x))
+		return self.dense4(x)
+
+# replay buffer object
+class Replay():
+	def __init__(self, action_size, buffer_size, batch_size, seed):
+		self.action_size = action_size
+		self.memory = deque(maxlen=buffer_size)
+		self.batch_size = batch_size
+		self.experiences = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
+		self.seed = random.seed(seed)
+
+	def add(self, state, action, reward, next_state, done):
+		e = self.experiences(state, action, reward, next_state, done)
+		self.memory.append(e)
+
+	def sample(self):
+		""" randomly sample experiences from memory """
+		experiences = random.sample(self.memory, k=self.batch_size)
+      
+		states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
+		actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).long().to(device)
+		rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
+		next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
+		dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
+
+		return (states,actions,rewards,next_states,dones)
+
+	def __len__(self):
+		""" get current size of samples in memory """
+		return len(self.memory)
+
+# DQN Agent
+class DQN_Agent():
+	def __init__(self, state_size, action_size, learning_rate, buffer_size, gamma, tau, batch_size, seed, soft_update):
+		self.state_size = state_size
+		self.action_size = action_size
+
+		# Intialise q-networks
+		self.qnet = QNet(state_size, action_size, seed).to(device)
+		self.qnet_target = QNet(state_size, action_size, seed).to(device)
+
+		# define optimiser
+		self.optimizer = optim.Adam(self.qnet.parameters(), lr=learning_rate)
+
+		# Replay Memory 
+		self.memory = Replay(action_size, buffer_size, batch_size, seed)
+		self.t_step = 0
+		self.soft_update_bool = soft_update        
+
+	def step(self, state, action, reward, next_step, update, batch_size, gamma, tau, done):
+
+		# save expereince in model
+		self.memory.add(state, action, reward, next_step, done)
+
+		# learn every 'x' time-steps
+		self.t_step = (self.t_step+1) % update
+
+		if self.soft_update_bool == True:
+			if self.t_step == 0:
+				if len(self.memory) > batch_size:
+					experience = self.memory.sample()
+					self.learn(experience, gamma, tau)
+		else:
+			if len(self.memory) > batch_size:
+				experience = self.memory.sample()
+				self.learn(experience, gamma, tau)
+
+
+	def action(self, state, epsilion = 0):
+		""" return action for given state given current policy """
+		state = torch.from_numpy(state).float().unsqueeze(0).to(device)
+		self.qnet.eval()
+
+		with torch.no_grad():
+			action_values = self.qnet(state)
+
+		self.qnet.train()    
+
+		# action selection relative to greedy action selection
+		if random.random() > epsilion:
+			return np.argmax(action_values.cpu().data.numpy())
+		else:
+			return random.choice(np.arange(self.action_size))
+
+
+	def learn(self, experiences, gamma, tau):
+
+		states, actions, rewards, next_states, dones = experiences
+
+		criterion = torch.nn.MSELoss()
+
+		# local model used to train
+		self.qnet.train()
+
+		# target model used in eval mode
+		self.qnet_target.eval()
+
+		predicted_targets = self.qnet(states).gather(1,actions)
+
+
+		with torch.no_grad():
+			labels_next = self.qnet_target(next_states).detach().max(1)[0].unsqueeze(1)
+ 
+
+		labels = rewards + (gamma * labels_next*(1-dones))
+
+		# print(labels_next)
+		# print(labels)
+		# print((1-dones))
+		# print(predicted_targets)
+		# exit()
+
+		loss = criterion(predicted_targets, labels).to(device)
+		# print(f"loss: {loss}---------------------------------------------------------------+++++++++++++++++++------")
+		self.optimizer.zero_grad()
+		loss.backward()
+		self.optimizer.step()
+
+		# now update the target next weights
+		if self.soft_update_bool == True:      
+			self.soft_update(self.qnet, self.qnet_target, tau)
+		elif (self.soft_update_bool == False) and self.t_step == 0:
+			self.soft_update(self.qnet, self.qnet_target, tau=1)
+
+	def soft_update(self, local_model, target_model, tau):
+
+		"""Soft update model parameters.
+        θ_target = τ*θ_local + (1 - τ)*θ_target """
+
+		for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
+			target_param.data.copy_(tau * local_param.data + (1 - tau) * target_param.data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
