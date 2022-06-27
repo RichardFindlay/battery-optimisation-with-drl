33c62c7fd865d6822be8b74a806adce716a502fe
Richard Findlay <rfindlay1992@gmail.com>
2022-06-27 23:42:24 +0100

    model updated to acommodate new features

--- DA_ElectricityPrice.py
+++ DA_ElectricityPrice.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 import numpy as np
 
 from pickle import load
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as pl
 
 
 # check for gpu compatibility
@@ -15,35 +15,12 @@ else:
   device = torch.device("cpu")
 
 
-#load training data dictionary
-train_set_load = open("./Data/processed_data/train_data_336hr_in_24hr_out.pkl", "rb") 
-train_set = load(train_set_load)
-train_set_load.close()
-
-
-# import data
-inputs = np.moveaxis(train_set['X_train'], -1, 1)
-y_true = train_set['y_train']
-# inputs = np.squeeze(train_set['X_train'],axis=-1)
-# y_true = np.squeeze(train_set['y_train'], axis=-1)
-
-inputs = torch.tensor(inputs, dtype=torch.float64)
-y_true = torch.tensor(y_true, dtype=torch.float64)
-
-inputs, y_true = inputs.cuda(), y_true.cuda()
-
-print(train_set['X_train'].shape)
-print(inputs.shape)
-# print(y_true.shape)
-
-
-
 # define DA price forecating model in Pytorch
 class LSTMCNNModel(nn.Module):
 	def __init__(self):
 		super(LSTMCNNModel ,self).__init__()
-		self.cnn = nn.Conv1d(6, 24, kernel_size=24, stride=24)
-		self.lstm = nn.LSTM(14, 32, 6, batch_first=True)
+		self.cnn = nn.Conv1d(9, 24, kernel_size=24, stride=24)
+		self.lstm = nn.LSTM(7, 32, 6, batch_first=True)
 		self.fc = nn.Linear(32, 1)
 		self.relu = nn.ReLU()
 		# self.device = device
@@ -55,12 +32,31 @@ class LSTMCNNModel(nn.Module):
 		return out
 
 
-model = LSTMCNNModel()
-model.cuda()
-
-
 
 def main():
+
+	#load training data dictionary
+	train_set_load = open("./Data/processed_data/train_data_336hr_in_24hr_out_2018_2019.pkl", "rb") 
+	train_set = load(train_set_load)
+	train_set_load.close()
+
+
+	# import data
+	inputs = np.moveaxis(train_set['X_train'], -1, 1)
+	y_true = train_set['y_train']
+	# inputs = np.squeeze(train_set['X_train'],axis=-1)
+	# y_true = np.squeeze(train_set['y_train'], axis=-1)
+
+	inputs = torch.tensor(inputs, dtype=torch.float64)
+	y_true = torch.tensor(y_true, dtype=torch.float64)
+
+	# inputs, y_true = inputs.cuda(), y_true.cuda()
+
+	# print(train_set['X_train'].shape)
+	# print(inputs.shape)
+	# print(y_true.shape)
+
+	model = LSTMCNNModel()
 
 	loss_fn = nn.MSELoss(reduction='sum')
 
@@ -88,7 +84,7 @@ def main():
 
 
 	# save trained model
-	PATH = './Models/da_price_prediction.pt'
+	PATH = './Models/da_price_prediction_2018.pt'
 	torch.save(model.state_dict(), PATH)
 
 
